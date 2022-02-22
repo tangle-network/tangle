449970c46f47995daecdd60f96e584fd5634cab3
Drew Stone <drewstone329@gmail.com>
2022-02-22 14:24:48 -0500

    Fixes

--- node/src/command.rs
+++ node/src/command.rs
@@ -29,7 +29,7 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn sc_service::ChainSpec>, St
 
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
-		"Parachain Collator Template".into()
+		"Webb Protocol Egg Network Collator".into()
 	}
 
 	fn impl_version() -> String {
@@ -38,7 +38,7 @@ impl SubstrateCli for Cli {
 
 	fn description() -> String {
 		format!(
-			"Parachain Collator Template\n\nThe command-line arguments provided first will be \
+			"Webb Protocol Egg Network Collator\n\nThe command-line arguments provided first will be \
 		passed to the parachain node, while the arguments provided after -- will be passed \
 		to the relaychain node.\n\n\
 		{} [parachain-args] -- [relaychain-args]",
@@ -69,7 +69,7 @@ impl SubstrateCli for Cli {
 
 impl SubstrateCli for RelayChainCli {
 	fn impl_name() -> String {
-		"Parachain Collator Template".into()
+		"Webb Protocol Egg Network Collator".into()
 	}
 
 	fn impl_version() -> String {
@@ -77,7 +77,7 @@ impl SubstrateCli for RelayChainCli {
 	}
 
 	fn description() -> String {
-		"Parachain Collator Template\n\nThe command-line arguments provided first will be \
+		"Webb Protocol Egg Network Collator\n\nThe command-line arguments provided first will be \
 		passed to the parachain node, while the arguments provided after -- will be passed \
 		to the relaychain node.\n\n\
 		dkg_node [parachain-args] -- [relaychain-args]"
