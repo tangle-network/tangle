4b034119e72c11eb6c8f513e57cb3f59413cc9cf
Drew Stone <drewstone329@gmail.com>
2022-02-24 13:21:20 -0500

    Updates to DKG

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -500,7 +500,6 @@ parameter_types! {
 	pub const PotId: PalletId = PalletId(*b"PotStake");
 	pub const MaxCandidates: u32 = 1000;
 	pub const MinCandidates: u32 = 5;
-	pub const SessionLength: BlockNumber = 6 * HOURS;
 	pub const MaxInvulnerables: u32 = 100;
 	pub const ExecutiveBody: BodyId = BodyId::Executive;
 }
