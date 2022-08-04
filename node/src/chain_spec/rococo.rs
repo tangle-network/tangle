8433ab00930f912f3f597cee6989fadf49d837da
1xstj <106580853+1xstj@users.noreply.github.com>
2022-08-04 10:55:42 +0530

    return unclaimed funds to selected destination (#54)
    * return to treasury
    
    * add test for claim fail
    
    * cargo fmt
    
    * clippy fixes

--- node/src/chain_spec/rococo.rs
+++ node/src/chain_spec/rococo.rs
@@ -117,7 +117,7 @@ fn rococo_genesis(
 				.expect("WASM binary was not build, please build it!")
 				.to_vec(),
 		},
-		claims: ClaimsConfig { claims: vec![], vesting: vec![] },
+		claims: ClaimsConfig { claims: vec![], vesting: vec![], expiry: None },
 		sudo: egg_rococo_runtime::SudoConfig { key: Some(root_key) },
 		balances: egg_rococo_runtime::BalancesConfig {
 			balances: endowed_accounts
