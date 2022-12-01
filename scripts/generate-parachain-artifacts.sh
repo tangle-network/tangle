fa0180d4e1aaf19f94b0fbe1764263f84d230307
1xstj <106580853+1xstj@users.noreply.github.com>
2022-12-01 22:49:48 +0400

    add parachain spec

--- scripts/generate-parachain-artifacts.sh
+++ scripts/generate-parachain-artifacts.sh
@@ -7,8 +7,8 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 cd "$PROJECT_ROOT"
 
 echo "****************** GENERATE RAW CHAINSPEC ******************"
-./target/release/tangle-parachain build-spec --disable-default-bootnode --chain tangle-dev --raw > ./chainspecs/tangle-parachain-chainspec.json
-./target/release/tangle-parachain export-genesis-state --chain ./chainspecs/tangle-parachain-chainspec.json > ./chainspecs/tangle-parachain-genesis-state
-./target/release/tangle-parachain export-genesis-wasm --chain ./chainspecs/tangle-parachain-chainspec.json > ./chainspecs/tangle-parachain-genesis-wasm
+./target/release/tangle-parachain build-spec --disable-default-bootnode --chain tangle-dev --raw > ./chainspecs/tangle-parachain.json
+./target/release/tangle-parachain export-genesis-state --chain ./chainspecs/tangle-parachain.json > ./chainspecs/tangle-parachain-genesis-state
+./target/release/tangle-parachain export-genesis-wasm --chain ./chainspecs/tangle-parachain.json > ./chainspecs/tangle-parachain-genesis-wasm
 
 popd
