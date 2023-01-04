ba4d1b4fd4f383f69c3e5289547cea04803b34b2
1xstj <106580853+1xstj@users.noreply.github.com>
2023-01-04 22:52:34 +0400

    bump version

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -141,7 +141,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-standalone"),
 	impl_name: create_runtime_str!("tangle-standalone"),
 	authoring_version: 1,
-	spec_version: 2,
+	spec_version: 100, // v0.1.0
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
