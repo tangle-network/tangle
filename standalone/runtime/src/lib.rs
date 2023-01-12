bab5c2666c4b39cc088e8ac92d518e082d09ca23
1xstj <106580853+1xstj@users.noreply.github.com>
2023-01-12 21:05:20 +0400

    remove asset-tx-payment

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -1112,15 +1112,6 @@ parameter_types! {
 // 	type Currency = Balances;
 // }
 
-impl pallet_asset_tx_payment::Config for Runtime {
-    type RuntimeEvent = RuntimeEvent;
-    type Fungibles = Assets;
-    type OnChargeAssetTransaction = pallet_asset_tx_payment::FungiblesAdapter<
-        pallet_assets::BalanceToAssetBalance<Balances, Runtime, ConvertInto>,
-        CreditToBlockAuthor,
-    >;
-}
-
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
