631df04a7d266faa11955e65a67f7e1fe9ef6b79
1xstj <106580853+1xstj@users.noreply.github.com>
2023-01-12 19:54:57 +0530

    bump dkg-substrate (#117)
    * generate updated chainspec
    
    * generate updated chainspec

--- node/src/chain_spec/rococo.rs
+++ node/src/chain_spec/rococo.rs
@@ -21,7 +21,7 @@ use sp_core::{crypto::UncheckedInto, sr25519};
 use tangle_rococo_runtime::{
 	AccountId, AssetRegistryConfig, AuraId, ClaimsConfig, DKGId, HasherBn254Config, ImOnlineConfig,
 	ImOnlineId, MerkleTreeBn254Config, MixerBn254Config, MixerVerifierBn254Config,
-	ParachainStakingConfig, VAnchorBn254Config, VAnchorVerifierConfig, MILLIUNIT, UNIT,
+	ParachainStakingConfig, VAnchorBn254Config, VAnchorVerifierConfig, UNIT,
 };
 
 pub fn tangle_alpha_config(id: ParaId) -> ChainSpec {
@@ -188,11 +188,7 @@ fn rococo_genesis(
 		claims: ClaimsConfig { claims: vec![], vesting: vec![], expiry: None },
 		sudo: tangle_rococo_runtime::SudoConfig { key: Some(root_key) },
 		balances: tangle_rococo_runtime::BalancesConfig {
-			balances: endowed_accounts
-				.iter()
-				.cloned()
-				.map(|k| (k, MILLIUNIT * 4_096_000))
-				.collect(),
+			balances: endowed_accounts.iter().cloned().map(|k| (k, 1_000_000_000 * UNIT)).collect(),
 		},
 		democracy: Default::default(),
 		council: Default::default(),
