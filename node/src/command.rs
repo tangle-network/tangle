709cb6026c5d72b1e8348ab0db82a2b9a453bf55
1xstj <106580853+1xstj@users.noreply.github.com>
2023-01-27 23:06:44 +0400

    cargo fmt

--- node/src/command.rs
+++ node/src/command.rs
@@ -27,9 +27,7 @@ use sc_cli::{
 	ChainSpec, CliConfiguration, DefaultConfigurationValues, ImportParams, KeystoreParams,
 	NetworkParams, Result, RuntimeVersion, SharedParams, SubstrateCli,
 };
-use sc_service::{
-	config::{BasePath, PrometheusConfig},
-};
+use sc_service::config::{BasePath, PrometheusConfig};
 use sp_core::hexdisplay::HexDisplay;
 use sp_runtime::traits::{AccountIdConversion, Block as BlockT};
 use std::net::SocketAddr;
