a0f1f12a0b642f2cf6248766275b8513862f88d7
1xstj <106580853+1xstj@users.noreply.github.com>
2023-02-10 13:04:04 +0400

    update ecdsa and increase session time to 6h

--- runtime/rococo/src/lib.rs
+++ runtime/rococo/src/lib.rs
@@ -161,7 +161,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-parachain"),
 	impl_name: create_runtime_str!("tangle-parachain"),
 	authoring_version: 1,
-	spec_version: 100, // v0.1.0
+	spec_version: 102, // v0.1.2
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
