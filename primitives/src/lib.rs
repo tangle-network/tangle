ab0e7114947b3825af79be748746b135c0f3df2c
Pavan Soratur <pavanvsoratur@gmail.com>
2023-03-07 10:45:08 -0800

    chore: updates session period to 6 hrs

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -110,7 +110,7 @@ pub mod fee {
 
 /// The number of blocks in one session
 #[allow(clippy::identity_op)]
-pub const SESSION_PERIOD_BLOCKS: BlockNumber = 1 * crate::time::HOURS;
+pub const SESSION_PERIOD_BLOCKS: BlockNumber = 6 * crate::time::HOURS;
 
 /// We assume that ~5% of the block weight is consumed by `on_initialize` handlers. This is
 /// used to limit the maximal weight of a single extrinsic.
