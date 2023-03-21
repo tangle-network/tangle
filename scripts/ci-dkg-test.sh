54624e3047b1b2953c448137c4e493bc1dc59279
1xstj <106580853+1xstj@users.noreply.github.com>
2023-03-21 23:49:05 +0400

    update ci-dkg-launch

--- scripts/ci-dkg-test.sh
+++ scripts/ci-dkg-test.sh
@@ -3,7 +3,55 @@ set -e
 
 # launch the standalone network
 echo "** Starting standalone network **"
-./scripts/run-standalone-local.sh --clean &
+
+echo "Cleaning tmp directory"
+rm -rf ./tmp
+rm -rf ./chainspecs/standalone-local.json
+
+# The following line ensure we run from the project root
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+cd "$PROJECT_ROOT"
+
+echo "** Generating Standalone local chainspec"
+./target/release/tangle-standalone build-spec --chain standalone-local > ./chainspecs/standalone-local.json
+
+echo "** Inserting keys **"
+./scripts/insert_keys.sh
+
+echo "*** Start Tangle Standalone | Standalone Local Config ***"
+# Node 1
+./target/release/tangle-standalone --base-path=./tmp/standalone1 -lerror --chain ./chainspecs/standalone-local.json --validator \
+  --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
+  --port 30304 \
+  --ws-port 9944 &
+# Node 2
+./target/release/tangle-standalone --base-path=./tmp/standalone2 -lerror --chain ./chainspecs/standalone-local.json --validator \
+  --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
+  --port 30305 \
+  --ws-port 9945 &
+# Node 3
+./target/release/tangle-standalone --base-path=./tmp/standalone3 -lerror --chain ./chainspecs/standalone-local.json --validator \
+  --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
+  --port 30306 \
+  --ws-port 9946 &
+# Node 4
+./target/release/tangle-standalone --base-path=./tmp/standalone4 -lerror --chain ./chainspecs/standalone-local.json --validator \
+  --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
+  --port 30307 \
+  --ws-port 9947 &
+# Node 5
+./target/release/tangle-standalone --base-path=./tmp/standalone5 -linfo --validator --chain ./chainspecs/standalone-local.json \
+    --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
+    --ws-port 9948 \
+    --port 30308 \
+    -ldkg=debug \
+    -ldkg_gadget::worker=debug \
+    -lruntime::dkg_metadata=debug \
+    -ldkg_metadata=debug \
+    -lruntime::dkg_proposal_handler=debug \
+    -lruntime::offchain=debug \
+    -ldkg_proposal_handler=debug \
+    -lruntime::im-online=debug &
 
 # wait for sometime for the network to be ready
 echo "** Waiting for testnet to start producing blocks **"
--- scripts/run-standalone-local.sh
+++ scripts/run-standalone-local.sh
@@ -3,16 +3,16 @@ set -e
 
 CLEAN=${CLEAN:-false}
 
-# define default ports
-# wstangleports=(9944 9945 9946 9947 9948)
+define default ports
+wstangleports=(9944 9945 9946 9947 9948)
 
-# check to see process is not orphaned or already running
-# for port in ${wstangleports[@]}; do
-#     if [[ $(lsof -i -P -n | grep LISTEN | grep :$port) ]]; then
-#       echo "Port $port has a running process. Exiting"
-#       exit -1
-#     fi
-# done
+check to see process is not orphaned or already running
+for port in ${wstangleports[@]}; do
+    if [[ $(lsof -i -P -n | grep LISTEN | grep :$port) ]]; then
+      echo "Port $port has a running process. Exiting"
+      exit -1
+    fi
+done
 
 # Parse arguments for the script
 
@@ -82,4 +82,4 @@ echo "*** Start Tangle Standalone | Standalone Local Config ***"
     -lruntime::offchain=debug \
     -ldkg_proposal_handler=debug \
     -lruntime::im-online=debug
-# popd
+popd
