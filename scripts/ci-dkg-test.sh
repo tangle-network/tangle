d9529c61ad55d9fe0817eeecf6bc82731164576a
1xstj <106580853+1xstj@users.noreply.github.com>
2023-03-24 19:44:38 +0400

    alternate gh exit

--- scripts/ci-dkg-test.sh
+++ scripts/ci-dkg-test.sh
@@ -23,27 +23,27 @@ echo "*** Start Tangle Standalone | Standalone Local Config ***"
 ./target/release/tangle-standalone --base-path=./tmp/standalone1 -lerror --chain ./chainspecs/standalone-local.json --validator \
   --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
   --port 30304 \
-  --ws-port 9944 &
+  --ws-port 9944  > ~/1.log 2>&1 &
 # Node 2
 ./target/release/tangle-standalone --base-path=./tmp/standalone2 -lerror --chain ./chainspecs/standalone-local.json --validator \
   --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
   --port 30305 \
-  --ws-port 9945 &
+  --ws-port 9945  > ~/2.log 2>&1 &
 # Node 3
 ./target/release/tangle-standalone --base-path=./tmp/standalone3 -lerror --chain ./chainspecs/standalone-local.json --validator \
   --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
   --port 30306 \
-  --ws-port 9946 &
+  --ws-port 9946  > ~/3.log 2>&1 &
 # Node 4
 ./target/release/tangle-standalone --base-path=./tmp/standalone4 -lerror --chain ./chainspecs/standalone-local.json --validator \
   --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
   --port 30307 \
-  --ws-port 9947 &
+  --ws-port 9947 > ~/4.log 2>&1 &
 # Node 5
 ./target/release/tangle-standalone --base-path=./tmp/standalone5 -linfo --validator --chain ./chainspecs/standalone-local.json \
     --rpc-cors all --unsafe-rpc-external --unsafe-ws-external \
     --ws-port 9948 \
-    --port 30308 &
+    --port 30308 > ~/5.log 2>&1 &
 
 # wait for sometime for the network to be ready
 echo "** Waiting for testnet to start producing blocks **"
@@ -58,6 +58,6 @@ if ! node index.js ; then exit 1 ; fi
 
 echo "** Liveness testing completed **"
 
-trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
+# trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
 
 exit 0
