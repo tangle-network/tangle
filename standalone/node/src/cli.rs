ea53a626bc87eee8b0fdd21c2e7ec544e7d30361
1xstj <106580853+1xstj@users.noreply.github.com>
2023-04-01 12:08:22 +0400

    fix clippy

--- standalone/node/src/cli.rs
+++ standalone/node/src/cli.rs
@@ -13,7 +13,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
-
+#![allow(clippy::all)]
 use sc_cli::RunCmd;
 
 #[derive(Debug, clap::Parser)]
