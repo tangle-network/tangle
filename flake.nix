fcf4710a0fb235a2ae6cb32fda84fa5c0461a5d3
Shady Khalifa <dev+github@shadykhalifa.me>
2023-05-04 13:37:50 +0300

    Setup Development env using Nix and Flakes

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Tangle development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # Rust
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        lib = pkgs.lib;
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "tangle";
+          nativeBuildInputs = [
+            pkgs.gmp
+            pkgs.protobuf
+            pkgs.pkg-config
+            # Needed for rocksdb-sys
+            pkgs.clang
+            pkgs.libclang.lib
+            pkgs.rustPlatform.bindgenHook
+            # Mold Linker for faster builds (only on Linux)
+            (lib.optionals pkgs.stdenv.isLinux pkgs.mold)
+          ];
+          buildInputs = [
+            # We want the unwrapped version, wrapped comes with nixpkgs' toolchain
+            pkgs.rust-analyzer-unwrapped
+            # Nodejs for test suite
+            pkgs.nodePackages.typescript-language-server
+            pkgs.nodejs_18
+            pkgs.nodePackages.yarn
+            # Finally the toolchain
+            toolchain
+          ];
+          packages = [ ];
+        };
+        # Environment variables
+        RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+      });
+}
