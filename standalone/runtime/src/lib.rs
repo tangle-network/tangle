53d197fbeff87b1fb241f2417066ef5701120058
1xstj <106580853+1xstj@users.noreply.github.com>
2023-06-01 21:40:36 +0400

    latest from signing-manager-debug

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -148,7 +148,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-standalone"),
 	impl_name: create_runtime_str!("tangle-standalone"),
 	authoring_version: 1,
-	spec_version: 129, // v0.1.29
+	spec_version: 130, // v0.1.30
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
