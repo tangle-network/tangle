776a34761cac5d48dc2033b6b54fd6e747482756
1xstj <106580853+1xstj@users.noreply.github.com>
2023-06-07 18:49:18 +0530

    thomas associated block changes

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -148,7 +148,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-standalone"),
 	impl_name: create_runtime_str!("tangle-standalone"),
 	authoring_version: 1,
-	spec_version: 131, // v0.1.31
+	spec_version: 132, // v0.1.32
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
