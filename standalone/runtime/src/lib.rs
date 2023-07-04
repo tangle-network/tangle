5cf7a589c60e5522e86596332b5e07995a4d27ac
1xstj <106580853+1xstj@users.noreply.github.com>
2023-07-04 15:31:13 +0530

    pull keygen changes

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -151,7 +151,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-standalone"),
 	impl_name: create_runtime_str!("tangle-standalone"),
 	authoring_version: 1,
-	spec_version: 203, // v0.2.3
+	spec_version: 204, // v0.2.4
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
