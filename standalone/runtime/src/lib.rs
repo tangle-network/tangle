aff08007490e763b771e90e2272ae2263e2dd469
1xstj <106580853+1xstj@users.noreply.github.com>
2023-07-10 18:56:15 +0530

    pull 0.2.6 from dkg

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-standalone"),
 	impl_name: create_runtime_str!("tangle-standalone"),
 	authoring_version: 1,
-	spec_version: 204, // v0.2.4
+	spec_version: 206, // v0.2.6
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
