b9b98ffa6970c55c09d57405f43d1aa7bc111ad9
salman01zp <pathansalman555@gmail.com>
2023-07-19 22:15:30 +0530

    cargo fmt

--- precompiles/proxy/src/tests.rs
+++ precompiles/proxy/src/tests.rs
@@ -15,7 +15,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use crate::mock::{
 	AccountId, ExtBuilder, PCall, PrecompilesValue, ProxyType, Runtime, RuntimeCall, RuntimeEvent,
 	RuntimeOrigin,
