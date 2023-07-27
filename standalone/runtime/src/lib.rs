1a56ee0ec76c065fa6e597d4fbfe57ecbdf802eb
Shady Khalifa <dev+github@shadykhalifa.me>
2023-07-27 17:32:26 +0300

    pump runtime version

--- standalone/runtime/src/lib.rs
+++ standalone/runtime/src/lib.rs
@@ -116,7 +116,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-standalone"),
 	impl_name: create_runtime_str!("tangle-standalone"),
 	authoring_version: 1,
-	spec_version: 303, // v0.3.3
+	spec_version: 304, // v0.3.4
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
