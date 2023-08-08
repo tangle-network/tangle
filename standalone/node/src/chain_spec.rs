9b283b4c6904f116b6d4f2f1a195c66e729dab0f
drewstone <drewstone329@gmail.com>
2023-08-07 23:56:11 -0400

    Testnet distribution (#245)

--- standalone/node/src/chain_spec.rs
+++ standalone/node/src/chain_spec.rs
@@ -14,8 +14,11 @@
 
 use std::{collections::BTreeMap, marker::PhantomData};
 
-use crate::testnet_fixtures::{
-	get_standalone_bootnodes, get_standalone_initial_authorities, get_testnet_root_key,
+use crate::{
+	distributions::{combine_distributions, develop, testnet},
+	testnet_fixtures::{
+		get_standalone_bootnodes, get_standalone_initial_authorities, get_testnet_root_key,
+	},
 };
 use dkg_runtime_primitives::{ResourceId, TypedChainId};
 use hex_literal::hex;
@@ -23,9 +26,8 @@ use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
 use sc_consensus_grandpa::AuthorityId as GrandpaId;
 use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_core::{sr25519, Pair, Public, H160, U256};
+use sp_core::{sr25519, Pair, Public, H160};
 use sp_runtime::traits::{IdentifyAccount, Verify};
-use std::str::FromStr;
 use tangle_runtime::{
 	AccountId, Balance, BalancesConfig, ClaimsConfig, DKGConfig, DKGId, DKGProposalsConfig,
 	EVMChainIdConfig, EVMConfig, ElectionsConfig, Eth2ClientConfig, ImOnlineConfig, MaxNominations,
@@ -160,6 +162,11 @@ pub fn development_config(chain_id: u64) -> Result<ChainSpec, String> {
 				chain_id,
 				DEFAULT_DKG_KEYGEN_THRESHOLD,
 				DEFAULT_DKG_SIGNATURE_THRESHOLD,
+				combine_distributions(vec![
+					develop::get_evm_balance_distribution(),
+					testnet::get_evm_balance_distribution(),
+				]),
+				testnet::get_substrate_balance_distribution(),
 				true,
 			)
 		},
@@ -233,6 +240,11 @@ pub fn local_testnet_config(chain_id: u64) -> Result<ChainSpec, String> {
 				chain_id,
 				DEFAULT_DKG_KEYGEN_THRESHOLD,
 				DEFAULT_DKG_SIGNATURE_THRESHOLD,
+				combine_distributions(vec![
+					develop::get_evm_balance_distribution(),
+					testnet::get_evm_balance_distribution(),
+				]),
+				testnet::get_substrate_balance_distribution(),
 				true,
 			)
 		},
@@ -308,6 +320,11 @@ pub fn relayer_testnet_config(chain_id: u64) -> Result<ChainSpec, String> {
 				chain_id,
 				relayer_testnet_dkg_keygen_threshold,
 				relayer_testnet_dkg_signature_threshold,
+				combine_distributions(vec![
+					develop::get_evm_balance_distribution(),
+					testnet::get_evm_balance_distribution(),
+				]),
+				testnet::get_substrate_balance_distribution(),
 				true,
 			)
 		},
@@ -367,6 +384,11 @@ pub fn standalone_live_config(chain_id: u64) -> Result<ChainSpec, String> {
 				chain_id,
 				DEFAULT_DKG_KEYGEN_THRESHOLD,
 				DEFAULT_DKG_SIGNATURE_THRESHOLD,
+				combine_distributions(vec![
+					develop::get_evm_balance_distribution(),
+					testnet::get_evm_balance_distribution(),
+				]),
+				testnet::get_substrate_balance_distribution(),
 				true,
 			)
 		},
@@ -440,6 +462,11 @@ pub fn standalone_testnet_config(chain_id: u64) -> Result<ChainSpec, String> {
 				chain_id,
 				DEFAULT_DKG_KEYGEN_THRESHOLD,
 				DEFAULT_DKG_SIGNATURE_THRESHOLD,
+				combine_distributions(vec![
+					develop::get_evm_balance_distribution(),
+					testnet::get_evm_balance_distribution(),
+				]),
+				testnet::get_substrate_balance_distribution(),
 				true,
 			)
 		},
@@ -498,6 +525,11 @@ pub fn standalone_local_config(chain_id: u64) -> Result<ChainSpec, String> {
 				chain_id,
 				DEFAULT_DKG_KEYGEN_THRESHOLD,
 				DEFAULT_DKG_SIGNATURE_THRESHOLD,
+				combine_distributions(vec![
+					develop::get_evm_balance_distribution(),
+					testnet::get_evm_balance_distribution(),
+				]),
+				testnet::get_substrate_balance_distribution(),
 				true,
 			)
 		},
@@ -530,6 +562,8 @@ fn testnet_genesis(
 	chain_id: u64,
 	dkg_keygen_threshold: u16,
 	dkg_signature_threshold: u16,
+	genesis_evm_distribution: Vec<(H160, fp_evm::GenesisAccount)>,
+	genesis_substrate_distribution: Vec<(AccountId, Balance)>,
 	_enable_println: bool,
 ) -> RuntimeGenesisConfig {
 	const ENDOWMENT: Balance = 10_000_000 * UNIT;
@@ -564,7 +598,12 @@ fn testnet_genesis(
 		sudo: SudoConfig { key: Some(root_key) },
 		balances: BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, ENDOWMENT)).collect(),
+			balances: endowed_accounts
+				.iter()
+				.cloned()
+				.map(|k| (k, ENDOWMENT))
+				.chain(genesis_substrate_distribution.iter().cloned().map(|(k, v)| (k, v)))
+				.collect(),
 		},
 		vesting: Default::default(),
 		indices: Default::default(),
@@ -625,125 +664,9 @@ fn testnet_genesis(
 		evm: EVMConfig {
 			accounts: {
 				let mut map = BTreeMap::new();
-				map.insert(
-					// H160 address of Alice dev account
-					// Derived from SS58 (42 prefix) address
-					// SS58: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-					// hex: 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
-					// Using the full hex key, truncating to the first 20 bytes (the first 40 hex
-					// chars)
-					H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
-							.expect("internal U256 is valid; qed"),
-						code: Default::default(),
-						nonce: Default::default(),
-						storage: Default::default(),
-					},
-				);
-				map.insert(
-					// H160 address for benchmark usage
-					H160::from_str("1000000000000000000000000000000000000001")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: vec![0x00],
-					},
-				);
-				// Other accounts used by relayer,bridges and tests
-				map.insert(
-					H160::from_str("8712c0449d1440d24a532a17c553e7661114e6bc")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("46Bf9B20A8144BaA7C2BB76303b6a17eB8755408")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("bFAc59575FeC3d1b33C7685eE6b3a2BfC155bdF3")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("c65351122A5dc7881559DeE52e025678212C615C")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("2ecceed83d6d2908cf4d67c76984e0bbcbfebbc1")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("228B67B0e42485E21373A7BB7278a0d02C8fDb18")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("5d26a601A80E3f472C5d6C3D1EdD78860F87Ac18")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("21Add37cBA50CF92A734c3Ee02FCeaDEf3dC57D6")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
-				map.insert(
-					H160::from_str("2DFA35bd8C59C38FB3eC4e71b0106160E130A40E")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: Default::default(),
-					},
-				);
+				for (address, account) in genesis_evm_distribution {
+					map.insert(address, account);
+				}
 				map
 			},
 			..Default::default()
---
+++ standalone/node/src/distributions/develop.rs
@@ -0,0 +1,128 @@
+use std::str::FromStr;
+
+use fp_evm::GenesisAccount;
+use sp_core::{H160, U256};
+
+pub fn get_evm_balance_distribution() -> Vec<(H160, GenesisAccount)> {
+	vec![
+		// H160 address of Alice dev account
+		// Derived from SS58 (42 prefix) address
+		// SS58: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
+		// hex: 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
+		// Using the full hex key, truncating to the first 20 bytes (the first 40 hex
+		// chars)
+		(
+			H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
+					.expect("internal U256 is valid; qed"),
+				code: Default::default(),
+				nonce: Default::default(),
+				storage: Default::default(),
+			},
+		),
+		// H160 address for benchmark usage
+		(
+			H160::from_str("1000000000000000000000000000000000000001")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: vec![0x00],
+			},
+		),
+		// Other accounts used by relayer, bridges, and tests
+		(
+			H160::from_str("8712c0449d1440d24a532a17c553e7661114e6bc")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("46Bf9B20A8144BaA7C2BB76303b6a17eB8755408")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("bFAc59575FeC3d1b33C7685eE6b3a2BfC155bdF3")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("c65351122A5dc7881559DeE52e025678212C615C")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("2ecceed83d6d2908cf4d67c76984e0bbcbfebbc1")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("228B67B0e42485E21373A7BB7278a0d02C8fDb18")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("5d26a601A80E3f472C5d6C3D1EdD78860F87Ac18")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("21Add37cBA50CF92A734c3Ee02FCeaDEf3dC57D6")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+		(
+			H160::from_str("2DFA35bd8C59C38FB3eC4e71b0106160E130A40E")
+				.expect("internal H160 is valid; qed"),
+			GenesisAccount {
+				nonce: U256::from(1),
+				balance: U256::from(1_000_000_000_000_000_000_000_000u128),
+				storage: Default::default(),
+				code: Default::default(),
+			},
+		),
+	]
+}
