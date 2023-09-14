ce7b761f11c95818e6c310e6351602ddd7dd7db8
Salman Pathan <pathansalman555@gmail.com>
2023-09-15 00:07:45 +0530

    Run light client relayer with tangle (#257)

--- standalone/node/src/cli.rs
+++ standalone/node/src/cli.rs
@@ -33,6 +33,10 @@ pub struct Cli {
 
 	#[clap(flatten)]
 	pub relayer_cmd: webb_relayer_gadget_cli::WebbRelayerCmd,
+
+	#[clap(flatten)]
+	pub light_client_relayer_cmd:
+		pallet_eth2_light_client_relayer_gadget_cli::LightClientRelayerCmd,
 }
 
 #[derive(Debug, clap::Subcommand)]
