48031dbc8fab436433b8cff5edeba54479594f11
1xstj <106580853+1xstj@users.noreply.github.com>
2023-10-27 12:09:13 -0500

    setup jobs pallet

---
+++ pallets/jobs/src/lib.rs
@@ -0,0 +1,117 @@
+// This file is part of Webb.
+// Copyright (C) 2021 Webb Technologies Inc.
+//
+// Tangle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tangle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Tangle.  If not, see <http://www.gnu.org/licenses/>.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(clippy::unused_unit)]
+
+use frame_support::{
+	dispatch::{CallMetadata, GetCallMetadata},
+	pallet_prelude::*,
+	traits::{Contains, PalletInfoAccess},
+	transactional,
+};
+use crate::types::JobSubmissionOf;
+use frame_system::pallet_prelude::*;
+use sp_runtime::DispatchResult;
+use sp_std::{prelude::*, vec::Vec};
+
+mod mock;
+mod tests;
+mod types;
+pub mod weights;
+pub use module::*;
+pub use weights::WeightInfo;
+
+#[frame_support::pallet]
+pub mod module {
+	use super::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		/// The origin which may set filter.
+		type UpdateOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+
+		/// Weight information for the extrinsics in this module.
+		type WeightInfo: WeightInfo;
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// can not pause
+		CannotPause,
+		/// invalid character encoding
+		InvalidCharacter,
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Paused transaction
+		TransactionPaused { pallet_name_bytes: Vec<u8>, function_name_bytes: Vec<u8> },
+		/// Unpaused transaction
+		TransactionUnpaused { pallet_name_bytes: Vec<u8>, function_name_bytes: Vec<u8> },
+	}
+
+	/// The paused transaction map
+	///
+	/// map (PalletNameBytes, FunctionNameBytes) => Option<()>
+	#[pallet::storage]
+	#[pallet::getter(fn paused_transactions)]
+	pub type PausedTransactions<T: Config> =
+		StorageMap<_, Twox64Concat, (Vec<u8>, Vec<u8>), (), OptionQuery>;
+
+	#[pallet::pallet]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T>(_);
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::call_index(0)]
+		#[pallet::weight(T::WeightInfo::pause_transaction())]
+		#[transactional]
+		pub fn submit_job(
+			origin: OriginFor<T>,
+			job : JobSubmissionOf<T>
+		) -> DispatchResult {
+			// T::UpdateOrigin::ensure_origin(origin)?;
+
+			// // not allowed to pause calls of this pallet to ensure safe
+			// let pallet_name_string =
+			// 	sp_std::str::from_utf8(&pallet_name).map_err(|_| Error::<T>::InvalidCharacter)?;
+			// ensure!(
+			// 	pallet_name_string != <Self as PalletInfoAccess>::name(),
+			// 	Error::<T>::CannotPause
+			// );
+
+			// PausedTransactions::<T>::mutate_exists(
+			// 	(pallet_name.clone(), function_name.clone()),
+			// 	|maybe_paused| {
+			// 		if maybe_paused.is_none() {
+			// 			*maybe_paused = Some(());
+			// 			Self::deposit_event(Event::TransactionPaused {
+			// 				pallet_name_bytes: pallet_name,
+			// 				function_name_bytes: function_name,
+			// 			});
+			// 		}
+			// 	},
+			// );
+			Ok(())
+		}
+
+	}
+}
