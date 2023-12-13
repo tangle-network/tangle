d71eae1ea3a892ef6d052d78dfc6e89e286fa519
drewstone <drewstone329@gmail.com>
2023-12-13 11:07:30 -0700

    Fix clippy deprecated warning + feature issues (#345)

--- relayer-gadget/cli/src/lib.rs
+++ relayer-gadget/cli/src/lib.rs
@@ -2,8 +2,8 @@ use std::path::PathBuf;
 
 /// Cli tool to interact with Webb Relayer CLI
 #[derive(Debug, Clone, clap::Parser)]
-#[clap(next_help_heading = "Webb Relayer")]
-pub struct WebbRelayerCmd {
+#[command(next_help_heading = "Webb Relayer")]
+pub struct RelayerCmd {
 	/// Directory that contains configration files for the relayer.
 	#[arg(long, value_name = "PATH")]
 	pub relayer_config_dir: Option<PathBuf>,
