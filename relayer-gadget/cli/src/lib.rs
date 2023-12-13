21f9577eefe07de56a517445ecfec47f87e34d97
Drew Stone <drewstone329@gmail.com>
2023-12-13 10:03:37 -0700

    Fix clippy deprecated warning

--- relayer-gadget/cli/src/lib.rs
+++ relayer-gadget/cli/src/lib.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 /// Cli tool to interact with Webb Relayer CLI
 #[derive(Debug, Clone, clap::Parser)]
-#[clap(next_help_heading = "Webb Relayer")]
+#[command(next_help_heading = "Webb Relayer")]
 pub struct WebbRelayerCmd {
 	/// Directory that contains configration files for the relayer.
 	#[arg(long, value_name = "PATH")]
