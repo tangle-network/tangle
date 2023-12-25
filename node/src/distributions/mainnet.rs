354586415e91f5bc2544f1f93a5ec667f3935ed2
shekohex <dev+github@shadykhalifa.me>
2023-12-25 17:04:51 +0000

    Add EVM Bridge Addresses to the distributions (#364)
    * Add EVM Bridge Addresses to the distributions
    
    * fmt

--- node/src/distributions/mainnet.rs
+++ node/src/distributions/mainnet.rs
@@ -53,6 +53,10 @@ pub fn get_faucet_evm_list() -> Vec<H160> {
 	read_contents_to_evm_accounts("node/src/distributions/data/evm_faucet_addresses.json")
 }
 
+pub fn get_bridge_evm_list() -> Vec<H160> {
+	read_contents_to_evm_accounts("node/src/distributions/data/evm_bridge_addresses.json")
+}
+
 pub fn get_faucet_substrate_list() -> Vec<AccountId32> {
 	super::testnet::read_contents_to_substrate_accounts(
 		"node/src/distributions/data/substrate_faucet_addresses.json",
@@ -129,6 +133,11 @@ pub fn get_leaderboard_balance_distribution() -> DistributionResult {
 		.map(|address| (MultiAddress::Native(address), ONE_HUNDRED_POINTS))
 		.collect();
 
+	let bridge_evm_list: Vec<(MultiAddress, u64)> = get_bridge_evm_list()
+		.into_iter()
+		.map(|address| (MultiAddress::EVM(EthereumAddress(address.0)), ONE_HUNDRED_POINTS))
+		.collect();
+
 	let leaderboard_points: Vec<(MultiAddress, u64)> = vec![];
 	// Chain all point lists together
 	let points_list = discord_list
@@ -136,6 +145,7 @@ pub fn get_leaderboard_balance_distribution() -> DistributionResult {
 		.chain(leaderboard_points)
 		.chain(faucet_evm_list)
 		.chain(faucet_substrate_list)
+		.chain(bridge_evm_list)
 		.collect::<Vec<(MultiAddress, u64)>>();
 	// Sum all the points
 	let total_points = points_list.iter().map(|(_, points)| points).sum::<u64>();
