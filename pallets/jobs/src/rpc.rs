14847f39727fd619bd8d4a5db8939a0d91381995
1xstj <106580853+1xstj@users.noreply.github.com>
2024-01-04 15:22:14 +0000

    feat : add jobs pallet rpc calls (#397)
    * feat : add jobs pallet rpc calls
    
    * fix merge

--- pallets/jobs/src/rpc.rs
+++ pallets/jobs/src/rpc.rs
@@ -1,6 +1,6 @@
 use super::*;
-use scale_info::prelude::string::String;
 pub use tangle_primitives::jobs::RpcResponseJobsData;
+use tangle_primitives::{jobs::RpcResponsePhaseOneResult, roles::RoleType};
 
 impl<T: Config> Pallet<T> {
 	/// Queries jobs associated with a specific validator.
@@ -20,10 +20,9 @@ impl<T: Config> Pallet<T> {
 	/// operation is successful, or an error message as a `String` if there is an issue.
 	pub fn query_jobs_by_validator(
 		validator: T::AccountId,
-	) -> Result<Vec<RpcResponseJobsData<T::AccountId>>, String> {
+	) -> Option<Vec<RpcResponseJobsData<T::AccountId>>> {
+		let mut jobs: Vec<RpcResponseJobsData<T::AccountId>> = vec![];
 		if let Some(jobs_list) = ValidatorJobIdLookup::<T>::get(validator) {
-			let mut jobs: Vec<RpcResponseJobsData<T::AccountId>> = vec![];
-
 			for (role_type, job_id) in jobs_list.iter() {
 				if let Some(job_info) = SubmittedJobs::<T>::get(role_type, job_id) {
 					if !job_info.job_type.is_phase_one() {
@@ -53,9 +52,103 @@ impl<T: Config> Pallet<T> {
 
 						jobs.push(info);
 					}
+				} else {
+					continue
 				}
 			}
 		}
-		Ok(vec![])
+
+		Some(jobs)
+	}
+
+	/// Queries a job by its key and ID.
+	///
+	/// # Arguments
+	///
+	/// * `role_type` - The role of the job.
+	/// * `job_id` - The ID of the job.
+	///
+	/// # Returns
+	///
+	/// An optional `RpcResponseJobsData` containing the account ID of the job.
+	pub fn query_job_by_id(
+		role_type: RoleType,
+		job_id: JobId,
+	) -> Option<RpcResponseJobsData<T::AccountId>> {
+		if let Some(job_info) = SubmittedJobs::<T>::get(role_type, job_id) {
+			if !job_info.job_type.is_phase_one() {
+				let result = KnownResults::<T>::get(
+					job_info.job_type.get_role_type(),
+					job_info.job_type.clone().get_phase_one_id().unwrap(),
+				)
+				.unwrap();
+
+				let info = RpcResponseJobsData::<T::AccountId> {
+					job_id,
+					job_type: job_info.job_type,
+					participants: result.participants(),
+					threshold: result.threshold(),
+					key: Some(result.result),
+				};
+
+				return Some(info)
+			} else {
+				let info = RpcResponseJobsData::<T::AccountId> {
+					job_id,
+					job_type: job_info.job_type,
+					participants: None,
+					threshold: None,
+					key: None,
+				};
+
+				return Some(info)
+			}
+		}
+
+		None
+	}
+
+	/// Queries the phase one result of a job by its key and ID.
+	///
+	/// # Arguments
+	///
+	/// * `role_type` - The role of the job.
+	/// * `job_id` - The ID of the job.
+	///
+	/// # Returns
+	///
+	/// An `Option` containing the phase one result of the job, wrapped in an
+	/// `RpcResponsePhaseOneResult`.
+	pub fn query_phase_one_by_id(
+		role_type: RoleType,
+		job_id: JobId,
+	) -> Option<RpcResponsePhaseOneResult<T::AccountId>> {
+		if let Some(job_info) = SubmittedJobs::<T>::get(role_type, job_id) {
+			if !job_info.job_type.is_phase_one() {
+				let result = KnownResults::<T>::get(
+					job_info.job_type.get_role_type(),
+					job_info.job_type.clone().get_phase_one_id().unwrap(),
+				)
+				.unwrap();
+
+				let info = RpcResponsePhaseOneResult::<T::AccountId> {
+					owner: job_info.owner,
+					result: result.result,
+					job_type: job_info.job_type,
+				};
+
+				return Some(info)
+			}
+		}
+
+		None
+	}
+
+	/// Queries next job ID.
+	///
+	///  # Returns
+	///  Next job ID.
+	pub fn query_next_job_id() -> JobId {
+		NextJobId::<T>::get()
 	}
 }
