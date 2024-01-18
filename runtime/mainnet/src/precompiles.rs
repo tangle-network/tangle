b5d500acb8d1089c3f264d54cc34e04ae18bd89e
salman01zp <pathansalman555@gmail.com>
2024-01-18 17:31:31 +0530

    fix imports

--- runtime/mainnet/src/precompiles.rs
+++ runtime/mainnet/src/precompiles.rs
@@ -23,6 +23,8 @@ use pallet_evm_precompile_registry::PrecompileRegistry;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
 use pallet_evm_precompile_staking::StakingPrecompile;
+use pallet_evm_precompile_vesting::VestingPrecompile;
+
 use precompile_utils::precompile_set::{
 	AcceptDelegateCall, AddressU64, CallableByContract, CallableByPrecompile, OnlyFrom,
 	PrecompileAt, PrecompileSetBuilder, SubcallWithMaxNesting,
@@ -49,7 +51,11 @@ pub type WebbPrecompilesAt<R> = (
 		StakingPrecompile<R>,
 		(CallableByContract, CallableByPrecompile),
 	>,
-	PrecompileAt<AddressU64<2049>, VestingPrecompile, (CallableByContract, CallableByPrecompile)>,
+	PrecompileAt<
+		AddressU64<2049>,
+		VestingPrecompile<R>,
+		(CallableByContract, CallableByPrecompile),
+	>,
 	// Moonbeam precompiles
 	PrecompileAt<
 		AddressU64<2051>,
