df238e95baeab120a1d44ea446eff95379b22544
Salman Pathan <pathansalman555@gmail.com>
2024-04-30 23:57:33 +0530

    Update ecdsa signature verification functions (#626)

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -1214,7 +1214,7 @@ parameter_types! {
 parameter_types! {
 	#[derive(Clone, RuntimeDebug, Eq, PartialEq, TypeInfo, Encode, Decode)]
 	#[derive(Serialize, Deserialize)]
-	pub const MaxSubmissionLen: u32 = 32;
+	pub const MaxSubmissionLen: u32 = 60_000_000;
 }
 
 parameter_types! {
