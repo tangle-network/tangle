15ce44f0b7dae55b42a7ce1692de424c035f4ff5
Drew Stone <drewstone329@gmail.com>
2024-05-06 16:04:50 -0400

    Version bujmp

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -171,7 +171,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle"),
 	impl_name: create_runtime_str!("tangle"),
 	authoring_version: 1,
-	spec_version: 1001, // v1.0.1
+	spec_version: 1002, // v1.0.2
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
