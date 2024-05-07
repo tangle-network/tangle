16d42b323c816646f0dd29a9e810ce4a9d60774c
shekohex <dev+github@shadykhalifa.me>
2024-05-07 20:32:17 +0300

    feat: pallet-services (#624)
    * feat: Jobs v2 Milestone 1 (#616)
    
    * add: pallet-services template
    
    * feat: pallet-services functions
    
    * adding tests
    
    * add more tests
    
    * working on service requests
    
    * service reqs
    
    * add: create service instances
    
    * feat: submit job calls and job result
    
    * add more tests
    
    * add more tests
    
    * feat: Jobs v2 Milestone 1 (#616)
    
    * add services precompile (cp from jobs precompile)
    
    * add `ServiceProviderPrefrences`
    
    * update the precompile
    
    * call into EVM
    
    * call into EVM
    
    * calling into EVM
    
    * feat: Jobs v2 Milestone 1 (#616)
    
    * Working on the demo
    
    * Making progress on Pallet => EVM
    
    * start using ethabi
    
    * validate job inputs using EVM
    
    * Address PR Reviews and issues

---
+++ precompiles/services/src/tests.rs
@@ -0,0 +1,64 @@
+// This file is part of Tangle.
+// Copyright (C) 2022-2024 Webb Technologies Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use crate::{mock::*, *};
+use precompile_utils::testing::*;
+use tangle_primitives::roles::ThresholdSignatureRoleType;
+
+fn precompiles() -> Precompiles<Runtime> {
+	PrecompilesValue::get()
+}
+
+#[test]
+fn submit_dkg_phase_one_job() {
+	ExtBuilder::default()
+		.with_balances(vec![(Alice.into(), 40)])
+		.build()
+		.execute_with(|| {
+			let _ = precompiles().prepare_test(
+				Address(CryptoAlith.into()),
+				Precompile1,
+				PCall::submit_dkg_phase_one_job {
+					role_type: RoleType::Tss(ThresholdSignatureRoleType::DfnsCGGMP21Secp256k1)
+						.into(),
+					expiry: 5,
+					ttl: 200,
+					participants: vec![],
+					threshold: 2,
+					permitted_caller: Address(CryptoAlith.into()),
+					hd_wallet: false,
+				},
+			);
+		})
+}
+
+#[test]
+fn submit_dkg_phase_two_job() {
+	ExtBuilder::default()
+		.with_balances(vec![(Alice.into(), 40)])
+		.build()
+		.execute_with(|| {
+			let _ = precompiles().prepare_test(
+				Address(CryptoAlith.into()),
+				Precompile1,
+				PCall::submit_dkg_phase_two_job {
+					expiry: 5,
+					ttl: 0,
+					phase_one_id: 1,
+					submission: vec![].into(),
+					derivation_path: vec![].into(),
+				},
+			);
+		})
+}
