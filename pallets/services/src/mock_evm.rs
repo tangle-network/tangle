f0b0588174445b80fc09026b1008a09c7f5223f8
Shady Khalifa <dev+github@shadykhalifa.me>
2024-05-09 17:06:20 +0300

    add `CustomEVMCurrencyAdapter`

--- pallets/services/src/mock_evm.rs
+++ pallets/services/src/mock_evm.rs
@@ -21,6 +21,9 @@ use crate::mock::{
 use ethereum::TransactionAction;
 use fp_ethereum::Transaction;
 use fp_evm::FeeCalculator;
+use frame_support::traits::{
+	Currency, ExistenceRequirement, Imbalance, OnUnbalanced, SignedImbalance, WithdrawReasons,
+};
 use frame_support::{parameter_types, traits::FindAuthor, weights::Weight, PalletId};
 use pallet_ethereum::{EthereumBlockHashMapping, IntermediateStateRoot, PostLogContent, RawOrigin};
 use pallet_evm::{
@@ -28,6 +31,7 @@ use pallet_evm::{
 	OnChargeEVMTransaction,
 };
 use sp_core::{keccak_256, ConstU32, H160, H256, U256};
+use sp_runtime::traits::UniqueSaturatedInto;
 use sp_runtime::{
 	traits::{BlakeTwo256, DispatchInfoOf, Dispatchable},
 	transaction_validity::{TransactionValidity, TransactionValidityError},
@@ -100,6 +104,114 @@ impl OnChargeEVMTransaction<Runtime> for FreeEVMExecution {
 	}
 
 	fn pay_priority_fee(tip: Self::LiquidityInfo) {}
+}
+
+/// Type alias for negative imbalance during fees
+type NegativeImbalanceOf<C> =
+	<C as Currency<<Runtime as frame_system::Config>::AccountId>>::NegativeImbalance;
+
+/// Implements the transaction payment for a pallet implementing the `Currency`
+/// trait (eg. the pallet_balances) using an unbalance handler (implementing
+/// `OnUnbalanced`).
+/// Similar to `CurrencyAdapter` of `pallet_transaction_payment`
+pub struct CustomEVMCurrencyAdapter<C, OU>(sp_std::marker::PhantomData<(C, OU)>);
+
+// See:
+// https://github.com/polkadot-evm/frontier/blob/c5d6daa9ffd46c0a85915526aa26d200fd635e30/frame/evm/src/lib.rs#L946
+impl<C, OU> OnChargeEVMTransaction<Runtime> for CustomEVMCurrencyAdapter<C, OU>
+where
+	C: Currency<<Runtime as frame_system::Config>::AccountId>,
+	C::PositiveImbalance: Imbalance<
+		<C as Currency<<Runtime as frame_system::Config>::AccountId>>::Balance,
+		Opposite = C::NegativeImbalance,
+	>,
+	C::NegativeImbalance: Imbalance<
+		<C as Currency<<Runtime as frame_system::Config>::AccountId>>::Balance,
+		Opposite = C::PositiveImbalance,
+	>,
+	OU: OnUnbalanced<NegativeImbalanceOf<C>>,
+	U256:
+		UniqueSaturatedInto<<C as Currency<<Runtime as frame_system::Config>::AccountId>>::Balance>,
+{
+	// Kept type as Option to satisfy bound of Default
+	type LiquidityInfo = Option<NegativeImbalanceOf<C>>;
+
+	fn withdraw_fee(
+		who: &H160,
+		fee: U256,
+	) -> Result<Self::LiquidityInfo, pallet_evm::Error<Runtime>> {
+		if fee.is_zero() {
+			return Ok(None);
+		}
+		let account_id = <Runtime as pallet_evm::Config>::AddressMapping::into_account_id(*who);
+		let imbalance = C::withdraw(
+			&account_id,
+			fee.unique_saturated_into(),
+			WithdrawReasons::FEE,
+			ExistenceRequirement::AllowDeath,
+		)
+		.map_err(|_| pallet_evm::Error::<Runtime>::BalanceLow)?;
+		Ok(Some(imbalance))
+	}
+
+	fn correct_and_deposit_fee(
+		who: &H160,
+		corrected_fee: U256,
+		base_fee: U256,
+		already_withdrawn: Self::LiquidityInfo,
+	) -> Self::LiquidityInfo {
+		if let Some(paid) = already_withdrawn {
+			let account_id = <Runtime as pallet_evm::Config>::AddressMapping::into_account_id(*who);
+
+			// Calculate how much refund we should return
+			let refund_amount = paid.peek().saturating_sub(corrected_fee.unique_saturated_into());
+			// refund to the account that paid the fees. If this fails, the
+			// account might have dropped below the existential balance. In
+			// that case we don't refund anything.
+			let refund_imbalance = C::deposit_into_existing(&account_id, refund_amount)
+				.unwrap_or_else(|_| C::PositiveImbalance::zero());
+
+			// Make sure this works with 0 ExistentialDeposit
+			// https://github.com/paritytech/substrate/issues/10117
+			// If we tried to refund something, the account still empty and the ED is set to 0,
+			// we call `make_free_balance_be` with the refunded amount.
+			let refund_imbalance = if C::minimum_balance().is_zero()
+				&& refund_amount > C::Balance::zero()
+				&& C::total_balance(&account_id).is_zero()
+			{
+				// Known bug: Substrate tried to refund to a zeroed AccountData, but
+				// interpreted the account to not exist.
+				match C::make_free_balance_be(&account_id, refund_amount) {
+					SignedImbalance::Positive(p) => p,
+					_ => C::PositiveImbalance::zero(),
+				}
+			} else {
+				refund_imbalance
+			};
+
+			// merge the imbalance caused by paying the fees and refunding parts of it again.
+			let adjusted_paid = paid
+				.offset(refund_imbalance)
+				.same()
+				.unwrap_or_else(|_| C::NegativeImbalance::zero());
+
+			let (base_fee, tip) = adjusted_paid.split(base_fee.unique_saturated_into());
+			// Handle base fee. Can be either burned, rationed, etc ...
+			OU::on_unbalanced(base_fee);
+			return Some(tip);
+		}
+		None
+	}
+
+	fn pay_priority_fee(tip: Self::LiquidityInfo) {
+		// Default Ethereum behaviour: issue the tip to the block author.
+		if let Some(tip) = tip {
+			let account_id = <Runtime as pallet_evm::Config>::AddressMapping::into_account_id(
+				<pallet_evm::Pallet<Runtime>>::find_author(),
+			);
+			let _ = C::deposit_into_existing(&account_id, tip.peek());
+		}
+	}
 }
 
 impl pallet_evm::Config for Runtime {
