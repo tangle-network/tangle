94cd393cebf4e4a2058aad89fba1871f48ae3143
shekohex <dev+github@shadykhalifa.me>
2024-05-10 21:19:15 +0300

    feat: Job Result Verification (#641)

--- forge/src/hooks/RequestHook.sol
+++ forge/src/hooks/RequestHook.sol
@@ -1,24 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity >=0.8.3;
 
-/// @dev Created by the service blueprint designer (gadget developer)
-contract RequestHook {
-    /// @dev Only allow the runtime to call this function.
-    modifier onlyRuntime() {
-      require(msg.sender == address(0x6D6F646c70792F73657276730000000000000000), "RegistrationHook: Only Runtime");
-      _;
-    }
+import "../Runtime.sol";
 
+/// @dev Created by the service blueprint designer (gadget developer)
+contract RequestHook is Runtime {
     /// @dev A Hook that gets called by the runtime when a User tries to request a service.
     /// @param requestInputs The inputs that the user provided during the service request.
     ///
     /// Unless this function reverts, the service will be created using this blueprint.
     /// @custom:hook
-    function onRequest(
-      uint64 serviceId,
-      bytes[] calldata participants,
-      bytes calldata requestInputs
-    ) public virtual payable onlyRuntime {}
+    function onRequest(uint64 serviceId, bytes[] calldata participants, bytes calldata requestInputs)
+        public
+        payable
+        virtual
+        onlyRuntime
+    {}
 
     /// @dev A Hook that gets called by the runtime when a User call a job on a the service.
     /// @param serviceId The id of the service.
@@ -27,10 +24,10 @@ contract RequestHook {
     /// @param inputs The inputs that the user provided during the job call.
     ///
     /// Unless this function reverts, the job will be executed using this service.
-    function onJobCall(
-      uint64 serviceId,
-      uint8 job,
-      uint64 jobCallId,
-      bytes calldata inputs
-    ) public virtual payable onlyRuntime {}
+    function onJobCall(uint64 serviceId, uint8 job, uint64 jobCallId, bytes calldata inputs)
+        public
+        payable
+        virtual
+        onlyRuntime
+    {}
 }
