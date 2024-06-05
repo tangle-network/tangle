d3256bea5a8cf715e19b46d7743444ca83de9625
Salman Pathan <pathansalman555@gmail.com>
2024-06-05 21:22:24 +0530

    Precompiles to verify Ecdsa signature (#669)
    * ecdsa-secp256k1 signature verifier precompile
    
    * add precompile to testnet runtime
    
    * add precompile to verify ecdsa-secp256r1 signature
    
    * add precompile to testnet runtime
    
    * update precompile address to 2071
    
    * add precompile to verify ecdsa-stark signature
    
    * add precompile to testnet runtime
    
    * clippy fixes

--- runtime/testnet/src/precompiles.rs
+++ runtime/testnet/src/precompiles.rs
@@ -25,6 +25,9 @@ use pallet_evm_precompile_registry::PrecompileRegistry;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
 use pallet_evm_precompile_staking::StakingPrecompile;
+use pallet_evm_precompile_verify_ecdsa_secp256k1_signature::EcdsaSecp256k1Precompile;
+use pallet_evm_precompile_verify_ecdsa_secp256r1_signature::EcdsaSecp256r1Precompile;
+use pallet_evm_precompile_verify_ecdsa_stark_signature::EcdsaStarkPrecompile;
 use pallet_evm_precompile_vesting::VestingPrecompile;
 
 use precompile_utils::precompile_set::{
@@ -118,6 +121,24 @@ pub type WebbPrecompilesAt<R> = (
 	PrecompileAt<
 		AddressU64<2069>,
 		PrecompileRegistry<R>,
+		(CallableByContract, CallableByPrecompile),
+	>,
+	// Ecdsa-Secp256k1 signature verifier precompile
+	PrecompileAt<
+		AddressU64<2070>,
+		EcdsaSecp256k1Precompile<R>,
+		(CallableByContract, CallableByPrecompile),
+	>,
+	// Ecdsa-Secp256r1 signature verifier precompile
+	PrecompileAt<
+		AddressU64<2071>,
+		EcdsaSecp256r1Precompile<R>,
+		(CallableByContract, CallableByPrecompile),
+	>,
+	// Ecdsa-Stark signature verifier precompile
+	PrecompileAt<
+		AddressU64<2072>,
+		EcdsaStarkPrecompile<R>,
 		(CallableByContract, CallableByPrecompile),
 	>,
 );
