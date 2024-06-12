0777b7c18e3d563f2b807c5fda44414581d59795
1xstj <106580853+1xstj@users.noreply.github.com>
2024-06-12 22:19:22 +0100

    bump version to v1.0.8

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -191,7 +191,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-testnet"),
 	impl_name: create_runtime_str!("tangle-testnet"),
 	authoring_version: 1,
-	spec_version: 1006, // v1.0.6
+	spec_version: 1008, // v1.0.8
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
