b880edc8441e2a1c83f12205b862c807bf171a8b
1xstj <106580853+1xstj@users.noreply.github.com>
2024-06-17 22:17:45 +0100

    update to v1.0.10 (#683)

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -171,7 +171,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle"),
 	impl_name: create_runtime_str!("tangle"),
 	authoring_version: 1,
-	spec_version: 1009, // v1.0.9
+	spec_version: 1010, // v1.0.10
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -191,7 +191,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-testnet"),
 	impl_name: create_runtime_str!("tangle-testnet"),
 	authoring_version: 1,
-	spec_version: 1009, // v1.0.9
+	spec_version: 1010, // v1.0.10
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
