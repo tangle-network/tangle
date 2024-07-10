ae4adcfaea584bd5ca9b715fd6d599c005f38634
salman01zp <pathansalman555@gmail.com>
2024-07-10 09:08:48 +0530

    fix account id conversion bug and add payout tests

--- precompiles/staking/src/tests.rs
+++ precompiles/staking/src/tests.rs
@@ -17,8 +17,9 @@
 // limitations under the License.
 
 use crate::mock::{
-	active_era, mock_pub_key, new_test_ext, start_session, Balances, PCall, Precompiles,
-	PrecompilesValue, Runtime, TestAccount,
+	active_era, make_all_reward_payment, mock_pub_key, new_test_ext, reward_all_elected,
+	start_active_era, start_session, Balances, PCall, Precompiles, PrecompilesValue, Runtime,
+	TestAccount,
 };
 use frame_support::traits::Currency;
 use pallet_staking::Nominators;
@@ -27,6 +28,12 @@ use sp_core::{H160, H256, U256};
 
 fn precompiles() -> Precompiles<Runtime> {
 	PrecompilesValue::get()
+}
+
+pub fn convert_h160_to_h256(address: H160) -> H256 {
+	let mut bytes = [0u8; 32];
+	bytes[12..32].copy_from_slice(&address.0);
+	H256(bytes)
 }
 
 #[test]
@@ -160,5 +167,141 @@ fn bond_should_work() {
 			)
 			.expect_no_logs()
 			.execute_returns(());
+	});
+}
+
+// Payout to stash account should work
+#[test]
+fn nominator_payout_to_stash_account_should_work() {
+	new_test_ext(vec![1, 2, 3, 4]).execute_with(|| {
+		let account = sp_core::sr25519::Public::from(TestAccount::Eve);
+
+		assert_eq!(Balances::total_balance(&account), 0);
+
+		// Lets give some tokens to account for bonding and nominating.
+		Balances::make_free_balance_be(&TestAccount::Eve.into(), 1_000_000_000u128);
+
+		// Lets Bond token with reward destination to Stash account
+		precompiles()
+			.prepare_test(
+				TestAccount::Eve,
+				H160::from_low_u64_be(1),
+				PCall::bond {
+					value: U256::from(1_000_000_000),
+					payee: H256([
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 2,
+					]),
+				},
+			)
+			.expect_no_logs()
+			.execute_returns(());
+
+		assert_eq!(Nominators::<Runtime>::get(account), None);
+
+		// Now lets nominate [Bobo,Dave,Charlie] for above account
+		let bob_address = sp_core::sr25519::Public::from(TestAccount::Bobo);
+		let dave_address = sp_core::sr25519::Public::from(TestAccount::Dave);
+		let charlie_address = sp_core::sr25519::Public::from(TestAccount::Charlie);
+
+		precompiles()
+			.prepare_test(
+				TestAccount::Eve,
+				H160::from_low_u64_be(1),
+				PCall::nominate {
+					targets: vec![
+						H256::from(bob_address),
+						H256::from(dave_address),
+						H256::from(charlie_address),
+					],
+				},
+			)
+			.expect_no_logs()
+			.execute_returns(());
+		let nominator = Nominators::<Runtime>::get(account).unwrap();
+		assert_eq!(nominator.targets.len(), 3);
+
+		// Nominator will be added in next era (era1) should have 0 usable balance.
+		assert_eq!(Balances::usable_balance(&account), 0);
+
+		// Reward payment for era 0, all the old validators should be rewarded.
+		reward_all_elected();
+		start_active_era(1);
+		make_all_reward_payment(0);
+
+		// Reward payment for era 1, Now the nominator should receive 10 rewards in its stash account
+		reward_all_elected();
+		start_active_era(2);
+		make_all_reward_payment(1);
+
+		assert_eq!(Balances::usable_balance(&account), 10);
+	});
+}
+
+// Payout to evm account should work
+#[test]
+fn nominator_payout_to_evm_account_should_work() {
+	new_test_ext(vec![1, 2, 3, 4]).execute_with(|| {
+		// We will be using this account to receive rewards
+		let evm_address = H160::repeat_byte(0x00);
+		let reward_address = convert_h160_to_h256(evm_address);
+		let mapped_substrate_account = sp_core::sr25519::Public::from(TestAccount::Empty);
+		assert_eq!(Balances::total_balance(&mapped_substrate_account), 0);
+
+		let account = sp_core::sr25519::Public::from(TestAccount::Eve);
+		assert_eq!(Balances::total_balance(&account), 0);
+
+		// Lets give some tokens to account for bonding and nominating.
+		Balances::make_free_balance_be(&TestAccount::Eve.into(), 1_000_000_000u128);
+
+		// Lets Bond token with reward destination to above evm address
+		precompiles()
+			.prepare_test(
+				TestAccount::Eve,
+				H160::from_low_u64_be(1),
+				PCall::bond { value: U256::from(1_000_000_000), payee: reward_address },
+			)
+			.expect_no_logs()
+			.execute_returns(());
+
+		assert_eq!(Nominators::<Runtime>::get(account), None);
+
+		// Now lets nominate [Bobo,Dave,Charlie] for above account
+		let bob_address = sp_core::sr25519::Public::from(TestAccount::Bobo);
+		let dave_address = sp_core::sr25519::Public::from(TestAccount::Dave);
+		let charlie_address = sp_core::sr25519::Public::from(TestAccount::Charlie);
+
+		precompiles()
+			.prepare_test(
+				TestAccount::Eve,
+				H160::from_low_u64_be(1),
+				PCall::nominate {
+					targets: vec![
+						H256::from(bob_address),
+						H256::from(dave_address),
+						H256::from(charlie_address),
+					],
+				},
+			)
+			.expect_no_logs()
+			.execute_returns(());
+		let nominator = Nominators::<Runtime>::get(account).unwrap();
+		assert_eq!(nominator.targets.len(), 3);
+
+		// Nominator will be added in next era (era1) should have 0 usable balance.
+		assert_eq!(Balances::usable_balance(&account), 0);
+
+		// Reward payment for era 0, all the old validators should be rewarded.
+		reward_all_elected();
+		start_active_era(1);
+		make_all_reward_payment(0);
+
+		// Reward payment for era 1, Now the nominator should receive 10 rewards in its reward destination address
+		reward_all_elected();
+		start_active_era(2);
+		make_all_reward_payment(1);
+
+		// Mapped substrate account evm address used for as reward destination.
+		assert_eq!(Balances::usable_balance(&mapped_substrate_account), 10);
 	});
 }
