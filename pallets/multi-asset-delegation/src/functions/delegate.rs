5bb090a204d690dedef254b7ffdecad02a0ff8db
1xstj <106580853+1xstj@users.noreply.github.com>
2024-07-26 14:25:37 +0100

    cargo fmt

--- pallets/multi-asset-delegation/src/functions/delegate.rs
+++ pallets/multi-asset-delegation/src/functions/delegate.rs
@@ -19,8 +19,8 @@ use crate::Pallet;
 use frame_support::ensure;
 use frame_support::pallet_prelude::DispatchResult;
 use frame_support::traits::Get;
-use sp_std::vec::Vec;
 use sp_runtime::traits::Zero;
+use sp_std::vec::Vec;
 
 impl<T: Config> Pallet<T> {
 	/// Processes the delegation of an amount of an asset to an operator.
