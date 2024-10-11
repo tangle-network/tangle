3b1d41726352ea987984e4ecb6c5cb74c25e80e3
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-10-11 23:19:14 +0700

    feat: export type bundles for Polkadot

--- types/src/index.ts
+++ types/src/index.ts
@@ -2,8 +2,42 @@
 // Copyright (C) 2022-2024 Webb Technologies Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export * from './interfaces'
-export * from './interfaces/augment-api.js'
-export * from './interfaces/augment-types.js'
-export * from './interfaces/definitions.js'
-export * from './interfaces/types-lookup.js'
+import './interfaces/augment-api'
+import './interfaces/augment-types'
+import './interfaces/types-lookup'
+
+import type {
+  OverrideBundleDefinition,
+  OverrideBundleType,
+} from '@polkadot/types/types'
+
+import * as tangleDefs from './interfaces/definitions'
+import { jsonrpcFromDefs, typesAliasFromDefs, typesFromDefs } from './utils'
+
+export * as tangleLookupTypes from './interfaces/lookup'
+
+export const tangleTypes = typesFromDefs(tangleDefs)
+export const tangleRpc = jsonrpcFromDefs(tangleDefs, {})
+export const tangleTypesAlias = typesAliasFromDefs(tangleDefs, {})
+
+const sharedBundle: OverrideBundleDefinition = {
+  rpc: tangleRpc,
+  types: [
+    {
+      minmax: [],
+      types: tangleTypes,
+    },
+  ],
+  alias: tangleTypesAlias,
+}
+
+export const tangleTypesBundle: OverrideBundleType = {
+  chain: {
+    tangle: sharedBundle,
+  },
+  spec: {
+    tangle: sharedBundle,
+  },
+}
+
+export const typesBundleForPolkadot = tangleTypesBundle
