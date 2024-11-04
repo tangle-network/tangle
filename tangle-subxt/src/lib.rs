a31c7886dc484db0370f6c08fc581f7bf249ceab
Serial <69764315+Serial-ATA@users.noreply.github.com>
2024-11-04 10:12:39 -0500

    feat(tangle-subxt): always export `subxt_core`

--- tangle-subxt/src/lib.rs
+++ tangle-subxt/src/lib.rs
@@ -14,7 +14,8 @@ pub use scale_info;
 #[cfg(any(feature = "std", feature = "web"))]
 pub use subxt;
 pub use subxt_signer;
-// `subxt` already re-exports `subxt-core`
+#[cfg(any(feature = "std", feature = "web"))]
+pub use subxt::ext::subxt_core;
 #[cfg(not(any(feature = "std", feature = "web")))]
 pub use subxt_core;
 
