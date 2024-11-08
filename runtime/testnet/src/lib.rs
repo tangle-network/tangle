de450a20bd1569e0491629a1a107cc89d352eb2a
1xstj <106580853+1xstj@users.noreply.github.com>
2024-11-08 16:30:09 +0000

    release v1.2.3

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -173,7 +173,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-testnet"),
 	impl_name: create_runtime_str!("tangle-testnet"),
 	authoring_version: 1,
-	spec_version: 1200, // v1.2.0
+	spec_version: 1203, // v1.2.3
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
