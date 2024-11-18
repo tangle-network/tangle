65a4f23b002170231652290dd4490d3b6ce587ac
Shady Khalifa <dev+github@shadykhalifa.me>
2024-11-18 17:13:01 +0200

    chore: add tnt-core

---
+++ contracts/src/MasterBlueprintServiceManager.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import "tnt-core/BlueprintServiceManagerBase.sol";
+
+contract MasterBlueprintServiceManager is BlueprintServiceManagerBase {}
