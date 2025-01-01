7ab3e6fd979890ceba5e5504e7e219799d155372
1xstj <106580853+1xstj@users.noreply.github.com>
2025-01-01 18:45:13 +0530

    wip

--- primitives/src/traits/mod.rs
+++ primitives/src/traits/mod.rs
@@ -2,8 +2,10 @@ pub mod assets;
 pub mod data_provider;
 pub mod multi_asset_delegation;
 pub mod services;
+pub mod rewards;
 
 pub use assets::*;
 pub use data_provider::*;
 pub use multi_asset_delegation::*;
 pub use services::*;
+pub use rewards::*;
