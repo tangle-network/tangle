0f792c935b3009926445e397206e40148d01154e
Shady Khalifa <dev+github@shadykhalifa.me>
2025-01-01 20:49:29 +0200

    feat: Mock ERC20 for testing

--- node/tests/evm_restaking.rs
+++ node/tests/evm_restaking.rs
@@ -1,23 +1,50 @@
 //! Runs only when `e2e` feature is enabled.
 #![cfg(feature = "e2e")]
 
+use alloy::network::EthereumWallet;
+use alloy::providers::Provider;
+use alloy::signers::local::PrivateKeySigner;
+use alloy::{providers::ProviderBuilder, sol};
 use common::run_e2e_test;
 use futures::prelude::*;
 use sp_tracing::info;
 use tangle_subxt::subxt::{self, OnlineClient};
+use tangle_subxt::subxt_signer::{ecdsa, sr25519, SecretUri};
 
 mod common;
+
+sol! {
+	#[sol(rpc)]
+	MockERC20,
+	"tests/fixtures/MockERC20.json",
+}
 
 #[test]
 fn it_works() {
 	run_e2e_test(async move {
-		let client = OnlineClient::<subxt::PolkadotConfig>::new().await.unwrap();
-		let stream = client.blocks().subscribe_best().await.unwrap();
-		let mut stream = stream.take(5);
-		while let Some(Ok(block)) = stream.next().await {
-			info!("Block: #{:?}", block.number());
-		}
+		let alice_seed = ecdsa::dev::alice().0.secret_bytes();
+		let signer = PrivateKeySigner::from_bytes((&alice_seed).into()).unwrap();
+		let alice_address = signer.address();
+		let wallet = EthereumWallet::from(signer);
+		let provider = ProviderBuilder::new()
+			.with_recommended_fillers()
+			.wallet(wallet)
+			.on_builtin("http://127.0.0.1:9944")
+			.await
+			.unwrap();
 
-		info!("It works!");
+		// Check the wallet balance
+		let balance = provider.get_balance(alice_address).await.unwrap();
+		info!("Balance: {}", balance);
+		// Deploy the contract at block #2.
+		let mut current_block = provider.get_block_number().await.unwrap();
+		info!("Current block: {}", current_block);
+		while current_block < 2 {
+			tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+			current_block = provider.get_block_number().await.unwrap();
+		}
+		info!("Deploying contract at block #{}", current_block);
+		let mock_erc20 = MockERC20::deploy(&provider).await.unwrap();
+		info!("Deployed contract at address: {}", mock_erc20.address());
 	});
 }
