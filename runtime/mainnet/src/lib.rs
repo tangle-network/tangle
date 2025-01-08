1485e8fa1637412d569bff6901b58c62aa0fcf72
1xstj <106580853+1xstj@users.noreply.github.com>
2025-01-08 10:41:08 +0530

    chore: release v1.2.5

--- runtime/mainnet/src/lib.rs
+++ runtime/mainnet/src/lib.rs
@@ -163,7 +163,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle"),
 	impl_name: create_runtime_str!("tangle"),
 	authoring_version: 1,
-	spec_version: 1204, // v1.2.4
+	spec_version: 1205, // v1.2.5
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
