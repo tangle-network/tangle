ba2e588a224750021ac2f3e72ac1ab0bb75f5aec
1xstj <106580853+1xstj@users.noreply.github.com>
2025-01-30 23:13:02 +0400

    clippy

--- node/tests/evm_restaking.rs
+++ node/tests/evm_restaking.rs
@@ -189,7 +189,8 @@ async fn deploy_tangle_lrt(
 
 // Mock values for consistent testing
 const EIGHTEEN_DECIMALS: u128 = 1_000_000_000_000_000_000_000;
-const MOCK_DEPOSIT_CAP: u128 = 1000 * EIGHTEEN_DECIMALS; // 1M tokens with 18 decimals
+const MOCK_DEPOSIT_CAP: u128 = 1_000_000 * EIGHTEEN_DECIMALS; // 1M tokens with 18 decimals
+const MOCK_DEPOSIT: u128 = 100_000 * EIGHTEEN_DECIMALS; // 100k tokens with 18 decimals
 const MOCK_APY: u8 = 10; // 10% APY
 
 /// Setup the E2E test environment.
@@ -297,7 +298,7 @@ where
 						api::runtime_types::pallet_rewards::types::RewardConfigForAssetVault {
 							apy: api::runtime_types::sp_arithmetic::per_things::Percent(MOCK_APY),
 							deposit_cap: MOCK_DEPOSIT_CAP,
-							incentive_cap: 0,
+							incentive_cap: 1,
 							boost_multiplier: Some(1),
 						},
 				},
@@ -923,7 +924,7 @@ fn lrt_deposit_withdraw_erc20() {
 }
 
 #[test]
-fn mad_rewards() {
+fn lrt_rewards() {
 	run_mad_test(|t| async move {
 		let alice = TestAccount::Alice;
 		let alice_provider = alloy_provider_with_wallet(&t.provider, alice.evm_wallet());
@@ -935,7 +936,7 @@ fn mad_rewards() {
 		let cfg_addr = api::storage().rewards().reward_config_storage(vault_id);
 		let cfg = t.subxt.storage().at_latest().await?.fetch(&cfg_addr).await?.unwrap();
 
-		let deposit = U256::from(tnt) * U256::from(2);
+		let deposit = U256::from(MOCK_DEPOSIT);
 
 		// Setup a LRT Vault for Alice.
 		let lrt_address = deploy_tangle_lrt(
@@ -951,43 +952,28 @@ fn mad_rewards() {
 		let bob = TestAccount::Bob;
 		let bob_provider = alloy_provider_with_wallet(&t.provider, bob.evm_wallet());
 		// Mint WETH for Bob
-		let usdc_amount = deposit;
-		let usdc = MockERC20::new(t.usdc, &bob_provider);
-		usdc.mint(bob.address(), usdc_amount).send().await?.get_receipt().await?;
+		let weth_amount = deposit;
+		let weth = MockERC20::new(t.weth, &bob_provider);
+		weth.mint(bob.address(), weth_amount).send().await?.get_receipt().await?;
 
-		// // Approve LRT contract to spend WETH
-		// let deposit_amount = weth_amount;
-		// let approve_result =
-		// 	weth.approve(lrt_address, deposit_amount).send().await?.get_receipt().await?;
-		// assert!(approve_result.status());
-		// info!("Approved {} WETH for deposit in LRT", format_ether(deposit_amount));
+		// Approve LRT contract to spend WETH
+		let deposit_amount = weth_amount;
+		let approve_result =
+			weth.approve(lrt_address, deposit_amount).send().await?.get_receipt().await?;
+		assert!(approve_result.status());
+		info!("Approved {} WETH for deposit in LRT", format_ether(deposit_amount));
 
-		// // Deposit WETH to LRT
-		// let lrt = TangleLiquidRestakingVault::new(lrt_address, &bob_provider);
-		// let deposit_result = lrt
-		// 	.deposit(deposit_amount, bob.address())
-		// 	.send()
-		// 	.await?
-		// 	.with_timeout(Some(Duration::from_secs(5)))
-		// 	.get_receipt()
-		// 	.await?;
-		// assert!(deposit_result.status());
-		// info!("Deposited {} WETH in LRT", format_ether(deposit_amount));
-
-		// Delegate assets
-		let precompile = MultiAssetDelegation::new(MULTI_ASSET_DELEGATION, &bob_provider);
-		let deposit_amount = U256::from(100_000_000u128);
-
-		// Deposit and delegate using asset ID
-		let deposit_result = precompile
-			.deposit(U256::from(t.usdc_asset_id), Address::ZERO, U256::from(deposit_amount), 0)
-			.from(bob.address())
+		// Deposit WETH to LRT
+		let lrt = TangleLiquidRestakingVault::new(lrt_address, &bob_provider);
+		let deposit_result = lrt
+			.deposit(deposit_amount, bob.address())
 			.send()
 			.await?
 			.with_timeout(Some(Duration::from_secs(5)))
 			.get_receipt()
 			.await?;
 		assert!(deposit_result.status());
+		info!("Deposited {} WETH in LRT", format_ether(deposit_amount));
 
 		// Wait for one year to pass
 		wait_for_more_blocks(&t.provider, 51).await;
