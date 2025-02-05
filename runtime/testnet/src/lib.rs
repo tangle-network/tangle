4b2cc6fc9134195a7dbfb68c4c3e10bf6a4c13b1
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-02-05 15:13:18 -0500

    chore: v1.2.8

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -169,7 +169,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("tangle-testnet"),
 	impl_name: create_runtime_str!("tangle-testnet"),
 	authoring_version: 1,
-	spec_version: 1207, // v1.2.7
+	spec_version: 1208, // v1.2.8
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
