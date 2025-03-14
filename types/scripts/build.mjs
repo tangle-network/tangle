4492cb0c868c2a8f3030685e518c0c9a9a9a530c
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2025-03-14 23:45:40 +0700

    refactor(types): update interface properties handling and add missing import for TanglePrimitivesServicesFieldFieldType

--- types/scripts/build.mjs
+++ types/scripts/build.mjs
@@ -1,10 +1,10 @@
 import { bundle } from "bunchee";
-import { readFile, writeFile } from "fs/promises";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 import { resolve } from "path";
 
-async function updateInterfaceProperties() {
+function updateInterfaceProperties() {
 	const filePath = resolve("src/interfaces/types-lookup.ts");
-	let content = await readFile(filePath, "utf-8");
+	let content = readFileSync(filePath, "utf-8");
 
 	// Replace properties in the IsmpRouterGetResponse interface
 	content = content.replace(
@@ -12,15 +12,51 @@ async function updateInterfaceProperties() {
 		"interface IsmpRouterGetResponse extends Struct {$1getRequest:$2;$3getValues:$4;$5}",
 	);
 
-	await writeFile(filePath, content);
+	writeFileSync(filePath, content);
 	console.log(
 		`✅ Updated IsmpRouterGetResponse interface properties inside ${filePath}`,
 	);
 }
 
+function addMissingImport() {
+	const filePath = resolve("src/interfaces/services/types.ts");
+	const importContent = `import { TanglePrimitivesServicesFieldFieldType } from '@polkadot/types/lookup';`;
+
+	// Check if the import exists
+	if (!existsSync(filePath)) {
+		console.warn(
+			`⚠️ ${filePath} does not exist, ignoring add missing import for TanglePrimitivesServicesFieldFieldType`,
+		);
+		return;
+	}
+
+	const content = readFileSync(filePath, "utf-8");
+
+	// Check if the import already exists
+	if (content.includes(importContent)) {
+		console.log(
+			`✅ Import for TanglePrimitivesServicesFieldFieldType already exists inside ${filePath}`,
+		);
+		return;
+	}
+
+	// Add the content at line 3
+	const lines = content.split("\n");
+	lines.splice(3, 0, importContent);
+	writeFileSync(filePath, lines.join("\n"));
+
+	console.log(
+		`✅ Added missing import for TanglePrimitivesServicesFieldFieldType inside ${filePath}`,
+	);
+}
+
 async function main() {
 	// Update interface properties before bundling
-	await updateInterfaceProperties();
+	updateInterfaceProperties();
+
+	// Add missing import for TanglePrimitivesServicesFieldFieldType
+	// Manually add until this issue is resolved: https://github.com/polkadot-js/api/issues/6117
+	addMissingImport();
 
 	await bundle("", {
 		clean: true,
