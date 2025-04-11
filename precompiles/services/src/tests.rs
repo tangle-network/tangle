d2a525ca85f56f4a67b8327f4a5987d387a5e313
Serial <69764315+Serial-ATA@users.noreply.github.com>
2025-04-11 10:48:42 -0400

    fix: update tests

--- precompiles/services/src/tests.rs
+++ precompiles/services/src/tests.rs
@@ -92,7 +92,7 @@ fn cggmp21_blueprint() -> ServiceBlueprint<ConstraintsOf<Runtime>> {
 		],
 		registration_params: bounded_vec![],
 		request_params: bounded_vec![],
-		gadget: Default::default(),
+		sources: Default::default(),
 		supported_membership_models: bounded_vec![
 			MembershipModelType::Fixed,
 			MembershipModelType::Dynamic,
