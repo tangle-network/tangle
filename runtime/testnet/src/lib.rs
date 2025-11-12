64347bd16e458e89d8dfd8910f468ee3463d59fb
Daniel Bui <79790753+danielbui12@users.noreply.github.com>
2025-11-12 22:15:59 +0700

    chore: resolve all clippy issues

--- runtime/testnet/src/lib.rs
+++ runtime/testnet/src/lib.rs
@@ -1144,6 +1144,7 @@ parameter_types! {
 #[derive(
 	Copy,
 	Clone,
+	Default,
 	Eq,
 	PartialEq,
 	Ord,
@@ -1155,15 +1156,11 @@ parameter_types! {
 	scale_info::TypeInfo,
 )]
 pub enum ProxyType {
+	#[default]
 	Any,
 	NonTransfer,
 	Governance,
 	Staking,
-}
-impl Default for ProxyType {
-	fn default() -> Self {
-		Self::Any
-	}
 }
 
 impl parity_scale_codec::DecodeWithMemTracking for ProxyType {}
